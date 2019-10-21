1398c88bd5242b70b7ed862a8e8a0aae72361fbc
Biswabijaya Samal <me@biswabijaya.com>
2019-10-21 18:54:49 +0530

    Update beautify-json.js

--- scripts/beautify-json.js
+++ scripts/beautify-json.js
@@ -1,3 +1,10 @@
+/**
+ * Function to beautifyJSON Ny simply Calling a function
+ * @author Biswabijaya Samal
+ * @module 'json'
+ */
+
+
 function beautifyJSON(data,outputDivID) {
 	document.getElementById(outputDivID).innerHTML = JSON.stringify(data, undefined, 2);
 }
